d62e55d8d71b6fa8affeaeb5e6109dd3307eeef2
zacksfF <ssssaasss571@gmail.com>
2025-01-25 20:04:07 +0100

    add config test

--- config/config_test.go
+++ config/config_test.go
@@ -1,2 +1,28 @@
 package config
 
+import (
+	"testing"
+
+	"github.com/zacksfF/Zackchain/util"
+)
+
+func setup() error {
+	err := ParseConfig("../config.json")
+	if err != nil {
+		return err
+	}
+	path := "../testConfig.json"
+	err = util.ParseLoggingConfig(path)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func TestConfig(t *testing.T) {
+	setup()
+	cfg := GetConfig()
+	if len(cfg.ContractAddress) == 0 {
+		t.Fatal("ContractAddress is empty")
+	}
+}
